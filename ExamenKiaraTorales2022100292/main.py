105afbfc6fe58da1e0ae001d15f8c85222c7faa6
KiaraBeln <kiarbelltorales@gmail.com>
2025-05-24 12:46:49 -0300

    Update main.py

--- ExamenKiaraTorales2022100292/main.py
+++ ExamenKiaraTorales2022100292/main.py
@@ -19,4 +19,4 @@ def cliente():
     return jsonify(resultado)
 
 if __name__ == '__main__':
-    app.run(host='localhost', debug = True, port = 5001)
+    app.run(host='localhost', debug = True, port = 5003)
